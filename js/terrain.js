39dc31dd047e0af7086002a03cf9cfbef3be8afe
ondras <none@none>
2012-11-01 22:09:25 +0100

    terrain

--- js/terrain.js
+++ js/terrain.js
@@ -1,11 +1,8 @@
 Game.Terrain = function() {
 	this._noise = new ROT.Noise.Simplex();
-	this._dirs = ROT.DIRS[8];
+	this._buildingSeed = ROT.RNG.getUniform();
 
-	this._wallThreshold = 0.3;
 	this._treeThreshold = -0.6;
-	
-	this._cached = {};
 }
 
 Game.Terrain.TYPE_NONE		= 0;
@@ -14,32 +11,27 @@ Game.Terrain.TYPE_TREE		= 2;
 Game.Terrain.TYPE_WALL		= 3;
 
 /**
- * @returns {object} Keys: type, 
+ * @returns {int}
  */
 Game.Terrain.prototype.get = function(x, y) {
-	var key = x+","+y;
-	if (key in this._cached) { return this._cached[key]; }
+	var tileSize = [10, 10];
+	var tileX = Math.floor(x/tileSize[0]);
+	var tileY = Math.floor(y/tileSize[1]);
 	
-	if (this._isLand(x, y)) { return Game.Terrain.TYPE_LAND; }
+	var centerX = tileX*tileSize[0] + tileSize[0]/2;
+	var centerY = tileY*tileSize[1] + tileSize[1]/2;
 	
-	var noise = this._noise.get(x/30, y/20);
-	if (noise > this._wallThreshold) {
-		/* if any of the neighbors is land, return wall */
-		for (var i=0;i<this._dirs.length;i++) {
-			var tx = x + this._dirs[i][0];
-			var ty = y + this._dirs[i][1];
-			if (this._isLand(tx, ty)) {
-				this._cached[key] = Game.Terrain.TYPE_WALL;
-				return Game.Terrain.TYPE_WALL;
-			}
-		}
+	var dist = Math.max(Math.abs(centerX - x), Math.abs(centerY - y));
+	if (dist == 3) {
+		return Game.Terrain.TYPE_WALL;
+	} else if (dist < 3) {
+		return Game.Terrain.TYPE_NONE;
+	} else {
+		var noise = this._noise.get(x/30, y/20);
+		if (noise < this._treeThreshold) { return Game.Terrain.TYPE_TREE; }
 	}
 	
-	if (noise < this._treeThreshold) {
-		return Game.Terrain.TYPE_TREE;
-	}
-	
-	return Game.Terrain.TYPE_NONE;
+	return Game.Terrain.TYPE_LAND;
 }
 
 Game.Terrain.prototype.set = function(x, y, type) {
